cb3c370b6ce44025128a1783cbaff8f78a3bcd09
SirGFM <gfmandaji@hotmail.com>
2021-07-09 08:32:44 -0300

    cmd/gobwas-ws-req-chat: Refactor chat using Go's HTTP handler and gobwas/ws
    Use `gobwas/ws` over Go's `*http.Server`, to be able to handle regular
    URLs and upgrade select URLs to a websocket.

--- cmd/gobwas-ws-req-chat/main.go
+++ cmd/gobwas-ws-req-chat/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
-    gows "github.com/gorilla/websocket"
+    "github.com/gobwas/ws"
+    "github.com/gobwas/ws/wsutil"
     "log"
+    "net"
     "net/http"
     "net/url"
     "os"
@@ -19,29 +21,55 @@ type message struct {
     from string
 }
 
+func newMessage(msg string, from string) message {
+    return message {
+        t: time.Now(),
+        msg: msg,
+        from: from,
+    }
+}
+
 type participant struct {
-    conn *gows.Conn
+    conn net.Conn
     name string
     last time.Time
     send chan message
 }
 
 func (p *participant) run() {
+    var buf [1]wsutil.Message
+    defer p.conn.Close()
+
     for {
-        typ, txt, err := p.conn.ReadMessage()
+        buf, err := wsutil.ReadClientMessage(p.conn, buf[:])
         if err != nil {
-            log.Printf("err: %+v", err)
+            log.Printf("Couldn't read: %+v", err)
             return
-        } else if typ != gows.TextMessage {
-            continue
         }
 
-        msg := message {
-            t: time.Now(),
-            msg: string(txt),
-            from: p.name,
+        for i := range buf {
+            data := &(buf[i])
+            switch data.OpCode {
+            case ws.OpClose:
+                log.Printf("Server closed the connection to %s", p.name)
+                return
+            case ws.OpPing:
+                // TODO Lock before ponging
+                err = wsutil.WriteServerMessage(p.conn, ws.OpPong, data.Payload)
+                if err != nil {
+                    log.Printf("Couldn't pong: %+v", err)
+                    return
+                }
+            case ws.OpPong:
+                // Do nothing
+                continue
+            case ws.OpText:
+                // Queue the message
+                p.send <- newMessage(string(data.Payload), p.name)
+            default:
+                log.Printf("Ignoring message of type: %+v", data.OpCode)
+            }
         }
-        p.send <- msg
     }
 }
 
@@ -64,9 +92,10 @@ func (r *room) run() {
                 continue
             }
 
-            err := p.conn.WriteMessage(gows.TextMessage, txt)
+            // TODO Lock before sending the message
+            err := wsutil.WriteServerMessage(p.conn, ws.OpText, txt)
             if err != nil {
-                log.Printf("err: %+v", err)
+                log.Printf("Couldn't send: %+v", err)
                 return
             }
             p.last = msg.t
@@ -79,18 +108,50 @@ func (r *room) run() {
 type runningServer struct {
     httpServer *http.Server
     rooms map[string]*room
-    upgrader gows.Upgrader
 }
 
-// ServeHTTP is called by Go's http package whenever a new HTTP request arrives
+func (s *runningServer) connChat(w http.ResponseWriter, req *http.Request, channel string, username string) {
+    // Try to update the connection to a websocket connection
+    conn, _, _, err := ws.UpgradeHTTP(req, w)
+    if err != nil {
+        log.Printf("Failed to upgrade to websocket: %+v", err)
+    }
+
+    // Add the participant to the room (creating it as necessary)
+    chatRoom, ok := s.rooms[channel]
+    if !ok {
+        chatRoom = &room {
+            newMsg: make(chan message, 1),
+            name: channel,
+        }
+        s.rooms[channel] = chatRoom
+        go chatRoom.run()
+    }
+
+    p := participant {
+        conn: conn,
+        name: username,
+        send: chatRoom.newMsg,
+    }
+    chatRoom.users = append(chatRoom.users, p)
+    go p.run()
+
+    msg := fmt.Sprintf("%s joined %s", username, channel)
+    log.Printf(msg)
+    chatRoom.newMsg <- newMessage(msg, "")
+}
+
 func (s *runningServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    var channel string
+    var username string
+
     // Normalize and strip the URL from its leading prefix (and slash)
     resUrl := path.Clean(req.URL.EscapedPath())
     if len(resUrl) > 0 && resUrl[0] == '/' {
         // NOTE: The first character must not be a '/' because of the split
         resUrl = resUrl[1:]
     } else if len(resUrl) == 1 && resUrl[0] == '.' {
-        // Clean converts an empty path into a single "."
+        // Clean converts an empty path into a single ""
         resUrl = ""
     }
 
@@ -99,45 +160,23 @@ func (s *runningServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     for _, p := range strings.Split(resUrl, "/") {
         cleanPath, err := url.PathUnescape(p)
         if err != nil {
-            log.Printf("err: %+v", err)
+            log.Printf("Failed to normalize the URL: %+v", err)
             return
         }
         urlPath = append(urlPath, cleanPath)
     }
 
-    if len(urlPath) >= 2 {
-        user := urlPath[len(urlPath)-1]
-        roomName := strings.Join(urlPath[:len(urlPath)-1], "|")
+    if len(urlPath) == 3 && urlPath[0] == "chat" {
+        channel = urlPath[1]
+        username = urlPath[2]
 
-        chatRoom, ok := s.rooms[roomName]
-        if !ok {
-            chatRoom = &room {
-                newMsg: make(chan message, 1),
-                name: roomName,
-            }
-            s.rooms[roomName] = chatRoom
-            go chatRoom.run()
-        }
-
-        conn, err := s.upgrader.Upgrade(w, req, nil)
-        if err != nil {
-            log.Printf("err: %+v", err)
-            return
-        }
-
-        p := participant {
-            conn: conn,
-            name: user,
-            send: chatRoom.newMsg,
-        }
-        chatRoom.users = append(chatRoom.users, p)
-        go p.run()
-
-        log.Printf("%s joined %s", user, roomName)
+        s.connChat(w, req, channel, username)
+    } else {
+        log.Printf("Regular URL: %+v", urlPath)
     }
 }
 
-// Halts the `http.Server`, if still running
+// Halts the server, if still running
 func (s *runningServer) Close() {
     if s.httpServer != nil {
         s.httpServer.Close()
@@ -145,25 +184,15 @@ func (s *runningServer) Close() {
     }
 }
 
-func yes(r *http.Request) bool {
-    return true
-}
-
 func main() {
-    var srv runningServer
-
     log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
 
+    var srv runningServer
     srv.httpServer = &http.Server {
         Addr: "0.0.0.0:8888",
-        Handler: &srv,
+      Handler: &srv,
     }
     srv.rooms = make(map[string]*room)
-    srv.upgrader = gows.Upgrader {
-        ReadBufferSize:  1024,
-        WriteBufferSize: 1024,
-        CheckOrigin: yes,
-    }
 
     intHndlr := make(chan os.Signal, 1)
     signal.Notify(intHndlr, os.Interrupt)
